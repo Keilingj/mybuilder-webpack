4c570b3529d1d1993829ba75ebe920dd6c8721ae
Keilingj <542854850@qq.com>
2021-01-07 20:40:06 +0800

    chore: update unit test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,8 +4,8 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/keilingzhuang/Desktop/mybuilder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/keilingzhuang/Desktop/mybuilder-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('mybuilder-webpack/test/smoke/template/src/index/index.js' > -1), true);
+        assert.equal(baseConfig.entry.search.indexOf('mybuilder-webpack/test/smoke/template/src/search/index.js' > -1), true);
     });
 });
 
